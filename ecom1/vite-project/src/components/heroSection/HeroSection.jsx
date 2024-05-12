678838f60d9d4616b97f13facd614f9f54c0a659
srinakasam19 <140834906+srinakasam19@users.noreply.github.com>
2024-05-12 23:34:53 +0530

    Add files via upload

---
+++ ecom1/vite-project/src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function HeroSection() {
+  return (
+    <div>
+    <img src="src/components/eco.jpeg" alt="" style={{ width: '100%' }}/>
+</div>
+  )
+}
+
+export default HeroSection
